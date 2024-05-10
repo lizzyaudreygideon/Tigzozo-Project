71c19cd3349ce74e2f11b5bc2e88f8f5c3085f61
audreylizzy <lizzyaudreygideon@gmail.com>
2024-05-10 15:47:45 -0700

    second commit

---
+++ style.js
@@ -0,0 +1,24 @@
+function myFun() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+ 
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+ 
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
